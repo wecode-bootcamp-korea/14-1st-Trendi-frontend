5bb567b43e036a7f5391f94cf10f4bed1e868c45
sewon <shin6403@gmail.com>
2020-11-16 22:53:06 +0900

    [Footer] 1. Footer 컴포넌트 작성중

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,59 @@
 import React, { Component } from 'react';
+import './Footer.scss';
+
+const FOOTER_BOTTOM = [
+  {
+    id: 1,
+    comment: '자주 묻는 질문',
+    img: 'https://www.brandi.co.kr/static/20.08.01/images/icon_footer_01.png',
+  },
+  {
+    id: 2,
+    comment: '카톡 플러스 친구',
+    img: 'https://www.brandi.co.kr/static/20.08.01/images/icon_footer_01.png',
+  },
+  {
+    id: 3,
+    comment: '페이스북',
+    img: 'https://www.brandi.co.kr/static/20.08.01/images/icon_footer_01.png',
+  },
+  {
+    id: 4,
+    comment: '인스타그램',
+    img: 'https://www.brandi.co.kr/static/20.08.01/images/icon_footer_01.png',
+  },
+];
 
 class Footer extends Component {
   render() {
     return (
       <div className="Footer">
-        <div className="none"></div>
+        <div className="none">
+          <div className="container">
+            <div className="customerService">
+              <div className="leftBox1">고객센터 1566-6575</div>
+              <div className="leftBox2">
+                <div className="time">
+                  영업시간 : AM 10:00 ~ PM 17:00(주말 및 공휴일 휴무)
+                </div>
+                <div className="time">점심시간 : PM 12:30 ~ PM 13:30</div>
+              </div>
+            </div>
+            <div className="rightBox">
+              {FOOTER_BOTTOM.map((el) => {
+                return (
+                  <>
+                    <div className="divImgBox">
+                      <img className="divImg" src={el.img} alt="사진"></img>
+                    </div>
+                    <div className="divImgBox1">{el.comment}</div>
+                  </>
+                );
+              })}
+            </div>
+          </div>
+          <div className="footerBottom"></div>
+        </div>
       </div>
     );
   }
--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Footer from './Components/Footer/Footer';
 import Main from './Pages/Main/Main';
 
 class Routes extends Component {
@@ -9,6 +10,7 @@ class Routes extends Component {
         <Switch>
           <Route exact path="/" component={Main} />
         </Switch>
+        <Footer />
       </Router>
     );
   }
