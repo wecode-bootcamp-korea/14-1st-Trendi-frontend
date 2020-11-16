19c3122ff115a76a32fa464e70525314810ff0b6
sewon <shin6403@gmail.com>
2020-11-16 20:56:05 +0900

    Footer 초기 작업 진행중

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 
 class Footer extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="Footer">
+        <div className="none"></div>
+      </div>
+    );
   }
 }
 
