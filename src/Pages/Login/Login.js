f85ae1d5f890439fe092d7dca506f524c8015912
appWon <korkrip11@gmail.com>
2020-11-18 19:45:55 +0900

    화면을 보기 위한 기초 commit

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./Login.scss";
 
 class Login extends Component {
+  InSingUp = () => {
+    this.props.history.push("/SignUp");
+  };
+
   render() {
     return (
-      <div>
-        
+      <div className="Login">
+        <div>
+          <h1>오늘 사면 내일 도착?</h1>
+          <h2>유료배송으로 내일 받는 트랜디 Login!!</h2>
+          <form>
+            <input className="inputTextbox" placeholder="아이디 입력" />
+            <input className="inputTextbox" placeholder="비밀번호 입력" />
+            <input className="LoginBtn" type="button" value="로그인" />
+            <input className="SignUpBtn" type="button" value="회원가입" onClick={this.InSingUp} />
+          </form>
+          <div className="boundaryLine"></div>
+          <div className="otherLogin">
+            <label>간편로그인 / 가입</label>
+            <div>
+              <img></img>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Main from './Pages/Main/Main';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Main from "./Pages/Main/Main";
+import Login from "./Pages/Login/Login";
+import SignUp from "./Pages/SignUp/SignUp";
+import MyPage from "./Pages/MyPage/MyPage";
+import SignUpInfo from "./Pages/SignUp/SignUpInfo";
 
 class Routes extends Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Main} />
+          <Route exact path="/" component={Login} />
+          <Route exact path="/SignUp" component={SignUp} />
+          <Route exact path="/MyPage" component={MyPage} />
+          <Route exact path="/SignUpInfo" component={SignUpInfo} />
         </Switch>
       </Router>
     );
