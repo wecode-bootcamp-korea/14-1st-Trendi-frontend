ea560811069bcfea0d5bfaa6752fb9e26938f592
appWon <korkrip11@gmail.com>
2020-11-18 20:32:45 +0900

    Login fest api 연동

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,13 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./Login.scss";
 
 class Login extends Component {
+  InSingUp = () => {
+    fetch("10.58.6.55:8000/user/signup", {
+      method: "post",
+      body: JSON.stringify({
+        // user_name : "신재훈",
+        // user
+      }),
+    });
+    this.props.history.push("/SignUp");
+  };
+
   render() {
     return (
-      <div>
-        
+      <div className="Login">
+        <div>
+          <h1>오늘 사면 내일 도착?</h1>
+          <h2>유료배송으로 내일 받는 트랜디 Login!!</h2>
+          <form>
+            <input className="inputTextbox" placeholder="아이디 입력" />
+            <input className="inputTextbox" placeholder="비밀번호 입력" />
+            <input className="LoginBtn" type="button" value="로그인" />
+            <input className="SignUpBtn" type="button" value="회원가입" onClick={this.InSingUp} />
+          </form>
+          <div className="boundaryLine"></div>
+          <div className="otherLogin">
+            <label>간편로그인 / 가입</label>
+            <div>
+              <img></img>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
-
 export default Login;
--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Main from './Pages/Main/Main';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Main from "./Pages/Main/Main";
+import Login from "./Pages/Login/Login";
 
 class Routes extends Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Main} />
+          <Route exact path="/" component={Login} />
         </Switch>
       </Router>
     );
