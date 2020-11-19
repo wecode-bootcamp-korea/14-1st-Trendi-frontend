587a35c7032d625477b5495b2f81dbf90f081f2b
appWon <korkrip11@gmail.com>
2020-11-19 19:16:17 +0900

    ADD : 회원가입 완료 레이 아웃 완료

---
+++ src/Pages/SignUp/Complete.js
@@ -0,0 +1,18 @@
+import react, { Component } from "react";
+import "./Complete.scss";
+
+class Complete extends Component {
+  render() {
+    return (
+      <div className="Complete">
+        <div>
+          <img src="images/signup_complete.png" alt="회원가입완료 이미지"></img>
+          <h1>트랜디 회원가입을 축하 드립니다!!</h1>
+          <div className="layoutLine"></div>
+          <input type="button" className="returnMain" value="바료 쇼핑하기" />
+        </div>
+      </div>
+    );
+  }
+}
+export default Complete;
--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import SignUp from "./Pages/SignUp/SignUp";
+import Complete from "./Pages/SignUp/Complete";
 
 class Routes extends Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={SignUp} />
+          <Route exact path="/" component={Complete} />
         </Switch>
       </Router>
     );
