0752508e813e74912bbcbc4a74955bf92c4eb767
appWon <korkrip11@gmail.com>
2020-11-19 11:30:52 +0900

    회원가입 정보입력 rest api 연동 및 회원가입 기능 테스트 완료 commit

--- src/Pages/SignUp/SignUpInfo.js
+++ src/Pages/SignUp/SignUpInfo.js
@@ -18,22 +18,14 @@ class SignUpInfo extends Component {
         state: false,
         value: "",
         check: (value) => {
-          return /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i.test(
-            value
-          )
-            ? true
-            : false;
+          return /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i.test(value) ? true : false;
         },
       },
       password: {
         state: false,
         value: "",
         check: (value) => {
-          return /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,19}$/.test(
-            value
-          )
-            ? true
-            : false;
+          return /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,19}$/.test(value) ? true : false;
         },
       },
       passwordCheck: {
@@ -47,9 +39,7 @@ class SignUpInfo extends Component {
         state: false,
         value: "",
         check: (value) => {
-          return /^[가-힣]{2,4}|[a-zA-Z]{2,10}\s[a-zA-Z]{2,10}$/.test(value)
-            ? true
-            : false;
+          return /^[가-힣]{2,4}|[a-zA-Z]{2,10}\s[a-zA-Z]{2,10}$/.test(value) ? true : false;
         },
       },
     };
@@ -57,11 +47,39 @@ class SignUpInfo extends Component {
   validationCheck = (e) => {
     const { name, value } = e.target;
     const isValidation = this.state[name].check(value);
+
+    if (name === "id" || name === "email") {
+      fetch(configData.Login, {
+        method: "post",
+        body: JSON.stringify({
+          nick_name: value,
+        }),
+      }).then((res) => console.log(res.json()));
+    }
+
     let prev = this.state[name];
     prev.state = isValidation;
     prev.value = value;
-    this.setState({ [name]: prev });
+    this.setState({
+      [name]: prev,
+    });
   };
+
+  SignUpSendInfo = (e) => {
+    const { id, email, password, userName } = this.state;
+    fetch("/signup/signup", {
+      method: "post",
+      body: JSON.stringify({
+        user_name: userName.value,
+        nick_name: id.value,
+        password: password.value,
+        email: email.value,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => (res.MESSAGE === "SUCCESS" ? console.log("완성") : console.log("실패")));
+  };
+
   render() {
     const { id, password, email, passwordCheck, userName } = this.state;
     return (
@@ -71,102 +89,43 @@ class SignUpInfo extends Component {
           <div>
             <div>
               <label>아이디</label>
-              <input
-                type="text"
-                name="id"
-                placeholder="아이디 입력"
-                onBlur={this.validationCheck}
-              />
-              <img
-                className={id.state ? "imgShow" : ""}
-                src="images/lock.png"
-                alt="id통과이미지"
-              />
+              <input type="text" name="id" placeholder="아이디 입력" onBlur={this.validationCheck} />
+              <img className={id.state ? "imgShow" : ""} src="images/lock.png" alt="id통과이미지" />
               <div className="errorMsg">
-                {id.value.length > 0
-                  ? id.state
-                    ? ""
-                    : "길이는 6~20 사이, 한글 및 특수문자는 사용하실수 없습니다."
-                  : ""}
+                {id.value.length ? (id.state ? "" : "길이는 6~20 사이, 한글 및 특수문자는 사용하실수 없습니다.") : ""}
               </div>
             </div>
             <div>
               <label>비밀번호</label>
-              <input
-                type="password"
-                name="password"
-                placeholder="비밀번호 입력"
-                onBlur={this.validationCheck}
-              />
-              <img
-                className={password.state ? "imgShow" : ""}
-                src="images/lock.png"
-                alt="비밀번호 패스 이미지"
-              />
+              <input type="password" name="password" placeholder="비밀번호 입력" onBlur={this.validationCheck} />
+              <img className={password.state ? "imgShow" : ""} src="images/lock.png" alt="비밀번호 패스 이미지" />
               <div className="errorMsg">
-                {password.value.length > 0
-                  ? password.state
-                    ? ""
-                    : "비밀번호는 최소 8자 이상, 특수문자, 대문자, 소문자를 입력해주세요"
-                  : ""}
+                {password.value.length ? (password.state ? "" : "비밀번호는 최소 8자 이상, 특수문자, 대문자, 소문자를 입력해주세요") : ""}
               </div>
             </div>
             <div>
               <label>비밀번호 재확인</label>
-              <input
-                type="password"
-                name="passwordCheck"
-                placeholder="비밀번호 확인"
-                onBlur={this.validationCheck}
-              />
-              <img
-                className={passwordCheck.state ? "imgShow" : ""}
-                src="images/lock.png"
-                alt="비밀번호 확인 패스 이미지"
-              />
+              <input type="password" name="passwordCheck" placeholder="비밀번호 확인" onBlur={this.validationCheck} />
+              <img className={passwordCheck.state ? "imgShow" : ""} src="images/lock.png" alt="비밀번호 확인 패스 이미지" />
               <div className="errorMsg">
-                {passwordCheck.value.length > 0
-                  ? passwordCheck.state
-                    ? ""
-                    : "비밀번호가 일치 하지 않습니다"
-                  : ""}
+                {passwordCheck.value.length ? (passwordCheck.state ? "" : "비밀번호가 일치 하지 않습니다") : ""}
               </div>
             </div>
             <div>
               <label>E-mail</label>
-              <input
-                type="text"
-                name="email"
-                placeholder="이메일 입력"
-                onBlur={this.validationCheck}
-              />
-              <div className="errorMsg">
-                {email.value.length > 0
-                  ? email.state
-                    ? ""
-                    : "이메일 주소를 다시 확인해주세요"
-                  : ""}
-              </div>
+              <input type="text" name="email" placeholder="이메일 입력" onBlur={this.validationCheck} />
+              <div className="errorMsg">{email.value.length ? (email.state ? "" : "이메일 주소를 다시 확인해주세요") : ""}</div>
             </div>
             <div>
               <label>이름</label>
-              <input
-                type="text"
-                name="userName"
-                placeholder="이름 입력"
-                onBlur={this.validationCheck}
-              />
+              <input type="text" name="userName" placeholder="이름 입력" onBlur={this.validationCheck} />
               <div className="errorMsg">
-                {userName.value.length > 0
-                  ? userName.state
-                    ? ""
-                    : "이름 숫자, 특수문자를 사용할 수 없습니다."
-                  : ""}
+                {userName.value.length ? (userName.state ? "" : "이름 숫자, 특수문자를 사용할 수 없습니다.") : ""}
               </div>
             </div>
           </div>
           <div className="boundaryLine"></div>
-          <input className="SignUpInfoBtn" type="button" value="다음" />
+          <input className="SignUpInfoBtn" type="button" name="signup" value="다음" onClick={this.SignUpSendInfo} />
         </form>
       </div>
     );
--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Main from "./Pages/Main/Main";
-import Login from "./Pages/Login/Login";
-import SignUp from "./Pages/SignUp/SignUp";
-import MyPage from "./Pages/MyPage/MyPage";
+// import Login from "./Pages/Login/Login";
+// import SignUp from "./Pages/SignUp/SignUp";
+// import MyPage from "./Pages/MyPage/MyPage";
 import SignUpInfo from "./Pages/SignUp/SignUpInfo";
 
 class Routes extends Component {
@@ -11,10 +11,7 @@ class Routes extends Component {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/SignUp" component={SignUp} />
-          <Route exact path="/MyPage" component={MyPage} />
-          <Route exact path="/SignUpInfo" component={SignUpInfo} />
+          <Route exact path="/" component={SignUpInfo} />
         </Switch>
       </Router>
     );
