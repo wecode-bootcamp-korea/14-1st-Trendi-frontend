25f3c4d4f78af7e230034a5cc448f529cf606a3c
sewon <shin6403@gmail.com>
2020-11-19 14:53:32 +0900

    [신세원] 1.Footer 코드리뷰 수정

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,6 +1,41 @@
 import React, { Component } from 'react';
 import './Footer.scss';
 
+class Footer extends Component {
+  render() {
+    return (
+      <div className="Footer">
+        <div className="none">
+          <div className="container">
+            <div className="customerService">
+              <div className="leftBox1">고객센터 1566-6575</div>
+              <div className="leftBox2">
+                <div className="time">영업시간 : AM 10:00 ~ PM 17:00(주말 및 공휴일 휴무)</div>
+                <div className="time">점심시간 : PM 12:30 ~ PM 13:30</div>
+              </div>
+            </div>
+            <div className="rightBox">
+              {FOOTER_BOTTOM.map((el) => {
+                return (
+                  <>
+                    <div className="snsBox">
+                      <img className="snsImg" src={el.img} alt="사진"></img>
+                    </div>
+                    <div className="snsComent">{el.comment}</div>
+                  </>
+                );
+              })}
+            </div>
+          </div>
+          <div className="footerBottom"></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
+
 const FOOTER_BOTTOM = [
   {
     id: 1,
@@ -23,40 +58,3 @@ const FOOTER_BOTTOM = [
     img: 'https://www.brandi.co.kr/static/20.08.01/images/icon_footer_04.png',
   },
 ];
-
-class Footer extends Component {
-  render() {
-    return (
-      <div className="Footer">
-        <div className="none">
-          <div className="container">
-            <div className="customerService">
-              <div className="leftBox1">고객센터 1566-6575</div>
-              <div className="leftBox2">
-                <div className="time">
-                  영업시간 : AM 10:00 ~ PM 17:00(주말 및 공휴일 휴무)
-                </div>
-                <div className="time">점심시간 : PM 12:30 ~ PM 13:30</div>
-              </div>
-            </div>
-            <div className="rightBox">
-              {FOOTER_BOTTOM.map((el) => {
-                return (
-                  <>
-                    <div className="divImgBox">
-                      <img className="divImg" src={el.img} alt="사진"></img>
-                    </div>
-                    <div className="divImgBox1">{el.comment}</div>
-                  </>
-                );
-              })}
-            </div>
-          </div>
-          <div className="footerBottom"></div>
-        </div>
-      </div>
-    );
-  }
-}
-
-export default Footer;
