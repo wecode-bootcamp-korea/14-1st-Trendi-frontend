3fc209f2d2e9d9d4400e485616b056e58edcba41
appWon <korkrip11@gmail.com>
2020-11-19 16:44:55 +0900

    UPDATA test : Login.js 서버연동 테스트

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -2,15 +2,58 @@ import React, { Component } from "react";
 import "./Login.scss";
 
 class Login extends Component {
-  InSingUp = () => {
-    fetch("10.58.6.55:8000/user/signup", {
+  constructor() {
+    super();
+    this.state = {
+      id: "",
+      password: "",
+    };
+  }
+
+  setLoginData = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  Sigin = () => {
+    const { id, password } = this.state;
+    fetch("http://10.58.1.4:8000/user/login", {
       method: "post",
       body: JSON.stringify({
-        // user_name : "신재훈",
-        // user
+        nick_name: id,
+        password: password,
       }),
-    });
-    this.props.history.push("/SignUp");
+    }).then((res) => res.json());
+    // .then((res) => {
+    //   if (res.TOKEN) {
+    //     document.cookie = "token_id" + "=" + res.TOKEN;
+    //   } else {
+    //     console.log("!11");
+    //   }
+    // });
+  };
+
+  kakaoSigin = (e) => {
+    fetch(
+      "https://kauth.kakao.com/oauth/authorize?client_id=0293d652aa9ceeae7cafb055c7a3bbb2&redirect_uri=http://10.58.7.185:3000&response_type=code",
+      { credentials: "include" }
+    )
+      .then((res) => {
+        console.log(res);
+        // if (res.redirected) {
+        //   window.location.href = res.url;
+        // }
+      })
+      .catch(function (err) {
+        console.info(err);
+      });
+    // .then((res) => {
+    //   if (res.TOKEN) {
+    //     document.cookie = "token_id" + "=" + res.TOKEN;
+    //   } else {
+    //     console.log("!11");
+    //   }
+    // })
   };
 
   render() {
@@ -20,17 +63,16 @@ class Login extends Component {
           <h1>오늘 사면 내일 도착?</h1>
           <h2>유료배송으로 내일 받는 트랜디 Login!!</h2>
           <form>
-            <input className="inputTextbox" placeholder="아이디 입력" />
-            <input className="inputTextbox" placeholder="비밀번호 입력" />
-            <input className="LoginBtn" type="button" value="로그인" />
-            <input className="SignUpBtn" type="button" value="회원가입" onClick={this.InSingUp} />
+            <input className="inputTextbox" name="id" placeholder="아이디 입력" onChange={this.setLoginData} />
+            <input className="inputTextbox" name="password" type="password" placeholder="비밀번호 입력" onChange={this.setLoginData} />
+            <input className="LoginBtn" type="button" value="카카오 로그인" onClick={this.kakaoSigin} />
+            <input className="LoginBtn" type="button" value="로그인" onClick={this.Sigin} />
+            <input className="SignUpBtn" type="button" value="회원가입" />
           </form>
           <div className="boundaryLine"></div>
           <div className="otherLogin">
             <label>간편로그인 / 가입</label>
-            <div>
-              <img></img>
-            </div>
+            <div></div>
           </div>
         </div>
       </div>
