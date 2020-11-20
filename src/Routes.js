66a3704c51ce61424ee90082dd3e6ba9508e0fa3
sewon <shin6403@gmail.com>
2020-11-20 20:08:05 +0900

    conflict 방지를 위한 코드 수정

--- src/Routes.js
+++ src/Routes.js
@@ -2,18 +2,15 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SignUp from './Pages/SignUp/SignUp';
 import Main from './Pages/Main/Main';
-import Nav from './Components/Nav/Nav';
-import Footer from './Components/Footer/Footer';
 
 class Routes extends Component {
   render() {
     return (
       <Router>
-        <Nav />
         <Switch>
+          <Route exact path="/main" component={Main} />
           <Route exact path="/" component={SignUp} />
         </Switch>
-        <Footer />
       </Router>
     );
   }
