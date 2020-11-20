39c4e2cf3a48032a199bfe0b6637c110b5666dd7
sewon <shin6403@gmail.com>
2020-11-20 19:29:13 +0900

    [신세원] 1.상품 디테일 페이지 기능 구현중

--- src/Components/DetailItem/DropDownBox.js
+++ src/Components/DetailItem/DropDownBox.js
@@ -1,49 +1,106 @@
 import React, { Component } from 'react';
 import Select from 'react-select';
 import './DropDownBox.scss';
+import OrderBox from './OrderBox';
 
 class DropDownBox extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      number: 0,
+      value: '',
+      size: '',
+      selectPrice: 16200,
+      defaultPrice: 0,
+      click: false,
+      click2: false,
+      item: [],
+      orderBox: [],
+      priceBox: 0,
+    };
   }
+
+  componentDidMount() {
+    this.setState({});
+  }
+  // 상품 클릭시 클릭 바뀌게
+  handleChange = (el) => {
+    const { click, number } = this.state;
+    this.setState({ number: number + 1, value: el.label });
+  };
+
+  //사이즈 클릭시 값이 누적되고 오더박스 카드가 생김
+  handleSizeCheck = (e) => {
+    // console.log('e는 뭐지?', e);
+    const { click2, selectPrice, item, value, click, orderBox } = this.state;
+    let arr = [...item];
+    arr.push(selectPrice);
+    // console.log('arr에 item[] 복사됨 : ', arr);
+    let totalPrice = arr.reduce((acc, obj) => acc + obj, 0);
+    // console.log('totalPrice에 값이 더해짐 :', totalPrice);
+    let orderBox2 = [...orderBox];
+    orderBox2.push({ value, size: e.value, priceBox: 16200 });
+    //중복된 배열이 있을때 걸러주는 기능 추가
+    const filterItem = orderBox2;
+    const result = (a, b) => {
+      const newArray = [];
+      const obj = {};
+      for (let i in a) {
+        obj[a[i][b]] = a[i];
+      }
+      for (let i in obj) {
+        newArray.push(obj[i]);
+      }
+      return newArray;
+    };
+    const uniqueArray = result(filterItem, 'size');
+
+    this.setState({
+      value,
+      size: e.label,
+      click2: !click2,
+      defaultPrice: selectPrice,
+      item: arr,
+      priceBox: totalPrice,
+      orderBox: uniqueArray,
+    });
+  };
+
   render() {
+    const { click, value, size, priceBox, click2, number } = this.state;
+    console.log('orderBox :', this.state.orderBox);
+
     return (
       <div className="DropDownBox">
         <form className="dropDownContainer">
-          <Select className="select1" options={COLLOR} defaultValue={COLLOR[0]} />
-          <Select className="select2" options={SIZE} defaultValue={SIZE[0]} />
+          <Select
+            className="select1"
+            onChange={this.handleChange}
+            options={COLLOR}
+            defaultValue={COLLOR[0]}
+          />
+          {number > 0 && (
+            <Select
+              className="select2"
+              onChange={this.handleSizeCheck}
+              options={SIZE}
+              defaultValue={SIZE[0]}
+            />
+          )}
         </form>
         <footer className="footer">
-          <div className="orderContainer">
-            <div className="orderWrap">
-              <div className="orderBox">
-                <div className="orderBox2">
-                  <div className="order">
-                    <div className="order1">블랙</div>
-                    <div className="delivery">일반배송</div>
-                  </div>
-                  <div className="buttonBox">
-                    <button>X</button>
-                  </div>
-                </div>
-                <div className="orderBottomBox">
-                  <div className="orderBottom">
-                    <button className="minus">-</button>
-                    <div className="number">1</div>
-                    <button className="plus">+</button>
-                  </div>
-                  <div className="orderPriceBox">
-                    <div className="orderPrice1">19,200</div>
-                    <div className="orderPrice2">원</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          {this.state.orderBox.map((el) => {
+            return <OrderBox value={el.value} size={el.size} priceBox={el.priceBox} />;
+          })}
+          {/* {click2 && <OrderBox value={value} size={size} priceBox={priceBox} />} */}
+
           <div className="wrapPrice">
             <div className="priceBox">
               <div className="priceComment">총 상품금액</div>
-              <div className="price">1000원</div>
+              <div className="priceBox2">
+                <div className="price">{priceBox}</div>
+                <div className="price">원</div>
+              </div>
             </div>
           </div>
           <div className="purchaseBox">
@@ -81,4 +138,16 @@ const SIZE = [
   { value: 'small', label: 'Small' },
   { value: 'medium', label: 'Medium' },
   { value: 'large', label: 'Large' },
+];
+
+const TEST = [
+  '블랙small',
+  '블랙medium',
+  '블랙large',
+  '옐로우small',
+  '옐로우medium',
+  '옐로우large',
+  '그레이small',
+  '그레이medium',
+  '그레이large',
 ];
---
+++ src/Components/DetailItem/OrderBox.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './OrderBox.scss';
+
+class OrderBox extends Component {
+  render() {
+    const { value, size, priceBox } = this.props;
+    // console.log('props.value', value);
+    // console.log('props.size', size);
+    // console.log('props.priceBox', priceBox);
+    // console.log('========================');
+    return (
+      <div className="orderContainer">
+        <div className="orderWrap">
+          <div className="orderBox">
+            <div className="orderBox2">
+              <div className="order">
+                <div className="order1">{value}</div>
+                <span className="order2">{size}</span>
+                <div className="delivery">일반배송</div>
+              </div>
+              <div className="buttonBox">
+                <button>X</button>
+              </div>
+            </div>
+            <div className="orderBottomBox">
+              <div className="orderBottom">
+                <button className="minus">-</button>
+                <div className="number">1</div>
+                <button className="plus">+</button>
+              </div>
+              <div className="orderPriceBox">
+                <div className="orderPrice1">{priceBox}</div>
+                <div className="orderPrice2">원</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default OrderBox;
