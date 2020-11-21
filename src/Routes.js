81b2f5976bab754b27da20f48097273f7cedc5b6
sewon <shin6403@gmail.com>
2020-11-21 20:45:21 +0900

    브랜치 이동을 위한 커밋

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-<<<<<<< HEAD
-import Footer from './Components/Footer/Footer';
-=======
 import SignUp from './Pages/SignUp/SignUp';
->>>>>>> main
 import Main from './Pages/Main/Main';
 
 class Routes extends Component {
