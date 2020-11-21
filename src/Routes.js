42b54edf2419710cba5886388dc7b2796d559948
sewon <shin6403@gmail.com>
2020-11-21 17:41:26 +0900

    브랜치 충돌 제거

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-<<<<<<< HEAD
 import Footer from './Components/Footer/Footer';
-=======
 import SignUp from './Pages/SignUp/SignUp';
->>>>>>> main
 import Main from './Pages/Main/Main';
 
 class Routes extends Component {
@@ -12,8 +9,9 @@ class Routes extends Component {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Main} />
+          <Route exact path="/main" component={Main} />
           <Route exact path="/" component={SignUp} />
+          <Route exact path="/footer" component={Footer} />
         </Switch>
       </Router>
     );
