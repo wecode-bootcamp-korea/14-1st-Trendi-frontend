e5b32323f73b9413876285a8ab3f2e351b4bf15f
sewon <shin6403@gmail.com>
2020-11-21 17:37:48 +0900

    바디 브랜치 메인 merge

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Nav from '../../Components/Nav/Nav';
 import SimpleSlider from './SimpleSlider';
+import './Main.scss';
 import '../../Components/Nav/Nav.scss';
 import './SimpleSlider.scss';
 
@@ -14,37 +15,27 @@ class Main extends Component {
   }
 
   componentDidMount() {
-    console.log('부모 componentDidMount');
-    fetch(API, { method: 'post', body: JSON.stringify({ category_num: 2, sub_category_num: 0 }) })
-      .then((res) => res.json())
-      .then((res) => {
-        console.log('res확인', res);
-        this.setState({ data: res.post });
-      });
+    // 원두님 api 붙여볼것
+    // console.log('부모 componentDidMount');
+    // fetch(API, { method: 'post', body: JSON.stringify({ category_num: 2, sub_category_num: 0 }) })
+    //   .then((res) => res.json())
+    //   .then((res) => {
+    //     console.log('res확인', res);
+    //     this.setState({ data: res.post });
+    //   });
+
+    fetch('http://localhost:3000/data/MOCK_DATA.json', {
+      method: 'get',
+    }).then((res) => this.setState({ data: res }));
   }
 
   render() {
-    // <<<<<<< HEAD
-    // <<<<<<< HEAD
-    //     const { data } = this.state;
-    //     console.log('data 데이터 확인', this.state.data);
-    //     if (this.state.data.length) {
-    //       console.log('', this.state.data);
-    //       return <div>{data && <ItemList MainAPI={data} />}</div>;
-    //     } else {
-    //       return '';
-    //     }
-    // =======
-    //     return <div></div>;
-    // >>>>>>> main
-    // =======
     return (
       <div className="Main">
         <Nav />
         <SimpleSlider />
       </div>
     );
-    // >>>>>>> main
   }
 }
 
