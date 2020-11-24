d8f8d96b1dea0d6789de3304eb3e0ca610d8a698
sewon <shin6403@gmail.com>
2020-11-24 19:24:33 +0900

    상품 리스트 select box 생성완료

--- src/Pages/CategoriePage/ShirtList.js
+++ src/Pages/CategoriePage/ShirtList.js
@@ -1,3 +1,4 @@
+import { TransferWithinAStationSharp } from '@material-ui/icons';
 import React, { Component } from 'react';
 import './ShirtList.scss';
 
@@ -5,6 +6,7 @@ class ShirtList extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      value: 'select',
       count: 30,
       toggle: false,
       haru: true,
@@ -22,19 +24,29 @@ class ShirtList extends Component {
     this.setState({ toggle: true, count: count + 20 });
   };
 
+  change = (e) => {
+    this.setState({ value: e.target.value });
+  };
+
   render() {
-    const { count, toggle } = this.state;
+    const { count, toggle, value } = this.state;
     let dataCut = this.props.data ? this.props.data.slice(0, count) : '';
     let clickBox = toggle ? 'blackBtn' : 'whiteBtn';
 
     return (
       <div className="ShirtList">
         <div className="bodyContainer">
-          <div className="comment"></div>
+          <div className="dropDownBox">
+            <select value={value} onChange={this.change}>
+              <option value="">판매량순</option>
+              <option value="">최신순</option>
+              <option value="">리뷰많은순</option>
+              <option value="">낮은가격순</option>
+            </select>
+          </div>
           <div className="Box">
             {dataCut.map((api) => {
               const haruImg = api.delivery ? '../../../images/haru2.png' : '../../../images/white.png';
-
               const splitStr = api.title.length >= 5 ? api.title.substr(0, 17) + '・・・' : '';
               return (
                 <article className="itemContainer" key={api.item_seller}>
