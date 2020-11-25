9e765dcce166288989df56219db899abc23a0581
appWon <korkrip11@gmail.com>
2020-11-25 16:32:40 +0900

    add : 기존 파일 삭제로 인한 다시 push

--- src/Routes.js
+++ src/Routes.js
@@ -1,18 +1,22 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Complete from "./Pages/SignUp/Complete";
 import React, { Component } from "react";
+import Complete from "./Pages/SignUp/Complete";
 import SignUp from "./Pages/SignUp/SignUp";
 import Main from "./Pages/Main/Main";
+import Nav from "./Components/Nav/Nav";
+import Footer from "./Components/Footer/Footer";
 
 class Routes extends Component {
   render() {
     return (
       <Router>
+        <Nav />
         <Switch>
           <Route exact path="/" component={Complete} />
           <Route exact path="/signup" component={SignUp} />
           <Route exact path="/main" component={Main} />
         </Switch>
+        <Footer />
       </Router>
     );
   }
