a84b5360ec8b374160e32e0834c7cafe3458ebb8
appWon <korkrip11@gmail.com>
2020-11-25 10:51:15 +0900

    update : 장바구니 레이아웃 완료

--- src/Pages/MyCart/MyCart.js
+++ src/Pages/MyCart/MyCart.js
@@ -6,41 +6,89 @@ import "./MyCart.scss";
 class MyCart extends Component {
   constructor() {
     super();
-    this.state = {
-      myCartData: {},
-    };
+    this.state = { allChekced: true };
   }
   componentDidMount() {
     fetch(configData.MOCK_DATA2)
       .then((res) => res.json())
       .then((res) => this.setState({ myCartData: res }));
   }
+
+  changeQuantity = (e, isData) => {
+    const myCartData = this.state.myCartData;
+    console.log(myCartData);
+    // console.log(e.target.dataset);
+    // console.log(isData);
+
+    // this.setState({ myCartData });
+  };
+
+  allCheckBtn = (e) => {
+    const myCartData = [...this.state.myCartData];
+    const targetState = e.target.checked;
+    for (let i in myCartData) {
+      myCartData[i].checked = targetState;
+    }
+    console.log(myCartData);
+    this.setState({ myCartData, allChekced: targetState });
+  };
   render() {
-    const { myCartData } = this.state;
-    // let filterSellert = {};
+    const { myCartData, allChekced } = this.state;
+    const totalPrice =
+      myCartData &&
+      myCartData
+        .reduce((acc, cur) => {
+          return acc + cur.price;
+        }, 0)
+        .toLocaleString();
 
-    let filterSellert = {};
-
-    // myCartData.forEach((el) => {
-    //   !filterSellert.hasOwnProperty(el.delivery)
-    //     ? (filterSellert[el.delivery] = [el])
-    //     : (filterSellert[el.delivery] = [...filterSellert[el.delivery], el]);
-    // });
+    let filterDelivery = {};
+    myCartData &&
+      myCartData.forEach((el, index) => {
+        !el.hasOwnProperty("checked") && (el["checked"] = true);
+        !filterDelivery.hasOwnProperty(el.delivery)
+          ? (filterDelivery[el.delivery] = [el])
+          : (filterDelivery[el.delivery] = [...filterDelivery[el.delivery], el]);
+      });
 
     return (
       <div className="MyCart">
         <form>
           <h1>장바구니</h1>
           <div className="allSelectLayout">
-            <div>
-              <label>
-                <input type="checkbox" />
-                전체선택
-              </label>
-            </div>
+            <label>
+              <input className="allChekbox" type="checkbox" checked={allChekced} onChange={this.allCheckBtn} />
+              전체선택
+            </label>
             <div className="allDelete">전체삭제</div>
           </div>
-          {/* <div className="productsKind">{myCartData && <SelleProduct filterSellert={filterSellert} />}</div> */}
+          <div className="productsList">
+            {myCartData &&
+              Object.keys(filterDelivery).map((el, index) => (
+                <>
+                  <div className="deliveryKind" key={index}>
+                    {!index && <img src="images/delivery.png" alt="하루빠른배송이미지" />}
+                    <span className={`${!index && "haruDelivery"}`}>{el}</span>
+                  </div>
+                  <SelleProduct products={filterDelivery[el]} key={index} changeQuantity={this.changeQuantity} />
+                </>
+              ))}
+          </div>
+          <div className="AmountForm">
+            <div className="formTitle">총 결제 예상 금액</div>
+            <div className="EstimatedAmount">
+              <span>
+                {`총 상품 금액  ${totalPrice}
+                  원 
+                + 총 배송비 0원`}
+              </span>
+
+              <label>
+                총 결제예상 금액 <span className="orderPrice">{totalPrice}</span> 원
+              </label>
+            </div>
+            <input value="구매하기" />
+          </div>
         </form>
       </div>
     );
--- src/Pages/MyCart/SelleProduct.js
+++ src/Pages/MyCart/SelleProduct.js
@@ -6,50 +6,52 @@ class SellerProduct extends Component {
     this.state = {};
   }
   render() {
-    const { Products } = this.props;
-
-    let haruProducts = {};
-    viewData &&
-      Object.keys(viewData).map((deliveryKey) =>
-        viewData[deliveryKey].forEach((el) =>
-          !haruProducts.hasOwnProperty(el.seller)
-            ? (haruProducts[el.seller] = [el])
-            : (haruProducts[el.seller] = [...haruProducts[el.seller], el])
-        )
-      );
+    const { products, changeQuantity } = this.props;
+    let filterSeller = {};
+    products.forEach((el) => {
+      !filterSeller.hasOwnProperty(el.seller)
+        ? (filterSeller[el.seller] = [el])
+        : (filterSeller[el.seller] = [...filterSeller[el.seller], el]);
+    });
 
     return (
       <div className="SellerProduct">
         <table>
           <colgroup>
-            <col width={500} />
+            <col width={500} align="center" />
             <col width={200} />
             <col width={100} />
           </colgroup>
-          {Object.keys(Products).map((keyValue) => (
+          {Object.keys(filterSeller).map((keyValue) => (
             <>
               <thead>
                 <tr>
-                  <td>{keyValue}</td>
-                  <td>수량</td>
-                  <td>주문금액</td>
+                  <td className="sellerName">{keyValue}</td>
+                  <td className="quantity">수량</td>
+                  <td className="price">주문금액</td>
                 </tr>
               </thead>
               <tbody>
-                {Products[keyValue].map((value, index) => (
-                  <tr>
+                {filterSeller[keyValue].map((value, index) => (
+                  <tr key={index}>
                     <td className="flexAlign">
-                      <input type="checkbox" />
-                      <img src={value.thumb_image_url} />
-                      <div>
-                        <label>{value.title}</label>
-                        <span></span>
+                      <input type="checkbox" checked={value.checked} />
+                      <img src={value.thumb_image_url} alt="상품이미지" />
+                      <div className="productDetail">
+                        <div className="productTitle">{value.title}</div>
+                        <div className="productOption">
+                          {value.color} {value.size}
+                        </div>
                       </div>
                     </td>
-                    <td>{value.delivery}</td>
-                    <td>
-                      <span>{value.price}원</span>
-                      <div>바로주문</div>
+                    <td className="quantityBtnForm">
+                      <input type="button" data-state="-" value="-" onChange={(el) => changeQuantity(el, value)} />
+                      <input className="quantityText" value={value.quantity} onChange={(el) => changeQuantity(el, value)} />
+                      <input type="button" data-state="+" value="+" onChange={(el) => changeQuantity(el, value)} />
+                    </td>
+                    <td className="orderFomr">
+                      <div className="productPrice">{value.price.toLocaleString()}원</div>
+                      <input className="orderButton" type="button" value="바로구매" />
                     </td>
                   </tr>
                 ))}
@@ -57,7 +59,7 @@ class SellerProduct extends Component {
             </>
           ))}
         </table>
-        <div>하루배송 상품 추가하기</div>
+        {!products[0].delivery && <input type="button" className="addHaruDelivery" value="하루배송 상품 추가하기" />}
       </div>
     );
   }
