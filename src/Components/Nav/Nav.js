ea3dbadaf482b05e43378dcf05176e85ad9a5eba
sewon <shin6403@gmail.com>
2020-11-26 11:33:16 +0900

    Fix: conflict resolved

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
-import ShoppingNav from "./ShoppingNav";
-import "./Nav.scss";
+import React, { Component } from 'react';
+import ShoppingNav from './ShoppingNav';
+import './Nav.scss';
 
 class Nav extends Component {
   constructor(props) {
@@ -9,20 +9,17 @@ class Nav extends Component {
       navLists: false,
     };
   }
-<<<<<<< HEAD
-=======
 
   componentDidMount() {
-    const user_name = localStorage.getItem("user_name");
+    const user_name = localStorage.getItem('user_name');
     user_name && this.setState({ user_name });
 
     this.setState({ user_name: localStorage.get });
-    fetch("http://10.58.3.61:8000/products/categories/2")
+    fetch('http://10.58.3.61:8000/products/categories/2')
       .then((res) => res.json())
       .then((res) => res.data);
   }
 
->>>>>>> main
   handleLeave = () => {
     this.setState({ navLists: false });
   };
@@ -32,7 +29,7 @@ class Nav extends Component {
   };
 
   pageChage = (e) => {
-    this.props.history.push("/Login");
+    this.props.history.push('/Login');
   };
 
   render() {
@@ -51,7 +48,11 @@ class Nav extends Component {
               <div className="searchContainer">
                 <input className="searchInput"></input>
                 <div className="magnifierBox">
-                  <img className="magnifier" src="https://www.brandi.co.kr/static/20.08.01/images/a-action-bar-icon-search-nor.png" alt="돋보기" />
+                  <img
+                    className="magnifier"
+                    src="https://www.brandi.co.kr/static/20.08.01/images/a-action-bar-icon-search-nor.png"
+                    alt="돋보기"
+                  />
                 </div>
               </div>
             </div>
@@ -88,17 +89,17 @@ class Nav extends Component {
 export default Nav;
 
 const NAV_LIST = [
-  { id: 1, title: "찜" },
-  { id: 2, title: "장바구니" },
-  { id: 3, title: "마이페이지" },
-  { id: 4, title: "로그인" },
+  { id: 1, title: '찜' },
+  { id: 2, title: '장바구니' },
+  { id: 3, title: '마이페이지' },
+  { id: 4, title: '로그인' },
 ];
 
 const NAV_BOTTOM = [
-  { id: 1, title: "홈" },
-  { id: 2, title: "랭킹" },
-  { id: 3, title: "하루배송" },
-  { id: 4, title: "쇼핑몰·마켓" },
-  { id: 5, title: "특가" },
-  { id: 6, title: "스토어" },
+  { id: 1, title: '홈' },
+  { id: 2, title: '랭킹' },
+  { id: 3, title: '하루배송' },
+  { id: 4, title: '쇼핑몰·마켓' },
+  { id: 5, title: '특가' },
+  { id: 6, title: '스토어' },
 ];
