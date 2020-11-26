d3ba36a85cfb5506d8fda6a6b12cc49806b3096f
sewon <shin6403@gmail.com>
2020-11-26 11:30:14 +0900

    브랜치 머지를 위한 임시 커밋

--- src/Components/DetailItem/DetailItem.js
+++ src/Components/DetailItem/DetailItem.js
@@ -7,6 +7,13 @@ import ItemInfo from '../ItemInfomationBar/ItemInfo';
 import './DetailItem.scss';
 
 class DetailItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+  }
+
   goToItemInfo = (e) => {
     let location = document.querySelector('.nio1').offsetTop;
     window.scrollTo({ top: location, behavior: 'smooth' });
@@ -15,7 +22,15 @@ class DetailItem extends Component {
     let location = document.querySelector('.nio2').offsetTop;
     window.scrollTo({ top: location, behavior: 'smooth' });
   };
+
+  componentDidMount() {
+    fetch('http://10.58.5.224:8000/products/3')
+      .then((res) => res.json())
+      .then((res) => this.setState({ data: res.product_detail }));
+  }
+
   render() {
+    const { data } = this.state;
     return (
       <div className="DetailItem">
         <div className="noneDetailItem">
@@ -26,13 +41,11 @@ class DetailItem extends Component {
             </div>
             <ItemInfomationBar goToItemInfo={this.goToItemInfo} goToReview={this.goToReview} />
             <ItemInfo />
-            <div className="nio">테스트</div>
-            <div className="nio">테스트</div>
-            <div className="nio">테스트</div>
-            <div className="nio1">상품정보</div>
-            <div className="nio">상품정보</div>
-            <div className="nio">테스트</div>
-            <div className="nio2">리뷰</div>
+
+            <div>
+              <img src={data.image_url} alt="사진" />
+            </div>
+            <div></div>
             <div className="nio">테스트</div>
             <div className="nio">테스트</div>
             <div className="nio">테스트</div>
--- src/Routes.js
+++ src/Routes.js
@@ -1,26 +1,13 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Nav from './Components/Nav/Nav';
 import Footer from './Components/Footer/Footer';
 import Main from './Pages/Main/Main';
-// import SignUp from './Pages/SignUp/SignUp';
+import SignUp from './Pages/SignUp/SignUp';
 import SignUpInfo from './Pages/SignUp/SignUpInfo';
 import MyPage from './Pages/MyPage/MyPage';
 import DetailItem from './Components/DetailItem/DetailItem';
 import Complete from './Pages/SignUp/Complete';
-=======
-import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Nav from "./Components/Nav/Nav";
-import Footer from "./Components/Footer/Footer";
-import Main from "./Pages/Main/Main";
-import SignUp from "./Pages/SignUp/SignUp";
-import SignUpInfo from "./Pages/SignUp/SignUpInfo";
-import MyPage from "./Pages/MyPage/MyPage";
-import DetailItem from "./Components/DetailItem/DetailItem";
-import Complete from "./Pages/SignUp/Complete";
->>>>>>> main
 
 class Routes extends Component {
   render() {
@@ -29,15 +16,9 @@ class Routes extends Component {
         <Nav />
         <Switch>
           <Route exact path="/" component={Main} />
-<<<<<<< HEAD
-          {/* <Route exact path="/signup" component={SignUp} /> */}
-          <Route exact path="/signupinfo" component={SignUpInfo} />
-          <Route exact path="/DetailItem/:id" component={DetailItem} />
-=======
           <Route exact path="/signup" component={SignUp} />
           <Route exact path="/signupinfo" component={SignUpInfo} />
-          <Route exact path="/DetailItem" component={DetailItem} />
->>>>>>> main
+          <Route exact path="/DetailItem/:id" component={DetailItem} />
           <Route exact path="/mypage" component={MyPage} />
           <Route exact path="/Complete" component={Complete} />
         </Switch>
