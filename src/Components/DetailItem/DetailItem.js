76e25f032e79d4d7e16fdd45c04ef83d25b17a33
sewon <shin6403@gmail.com>
2020-11-26 13:07:02 +0900

    뭔가 문제있음

--- src/Components/DetailItem/DetailItem.js
+++ src/Components/DetailItem/DetailItem.js
@@ -1,21 +1,49 @@
 import React, { Component } from 'react';
 import ItemSection from './ItemSection';
 import PictureBox from './PictureBox';
-import './DetailItem.scss';
+import ItemInfomationBar from './ItemInfomationBar';
+import ItemInfo from './ItemInfo';
 
 class DetailItem extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      data: [],
+    };
+  }
+
+  goToItemInfo = (e) => {
+    let location = document.querySelector('.nio1').offsetTop;
+    window.scrollTo({ top: location, behavior: 'smooth' });
+  };
+  goToReview = (e) => {
+    let location = document.querySelector('.nio2').offsetTop;
+    window.scrollTo({ top: location, behavior: 'smooth' });
+  };
+
+  componentDidMount() {
+    fetch('http://10.58.5.224:8000/products/3')
+      .then((res) => res.json())
+      .then((res) => this.setState({ data: res.product_detail }));
   }
 
   render() {
+    const { data } = this.state;
     return (
       <div className="DetailItem">
         <div className="noneDetailItem">
           <div className="itemBox">
-            <PictureBox />
-            <ItemSection />
+            <div className="componentWrap">
+              <PictureBox />
+              <ItemSection />
+            </div>
+            {/* <ItemInfomationBar goToItemInfo={this.goToItemInfo} goToReview={this.goToReview} />
+            <ItemInfo /> */}
           </div>
+          <div className="nio">테스트</div>
+          <div className="nio">테스트</div>
+          <div className="nio">테스트</div>
+          <div className="nio">테스트</div>
         </div>
       </div>
     );
